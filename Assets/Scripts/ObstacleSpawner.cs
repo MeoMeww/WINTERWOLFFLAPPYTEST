4585a477dcf88575ea0b0335f2d532f07c6b4b36
MewMeo <trongduc.hd95@gmail.com>
2023-03-31 23:10:18 +0700

    MeoMew resolve task #2: Use only 1 obstacle, the heigh of obstacle was change by step, if easy, the obstacle y change by previous obstacle random from 0.3 -> 1.0, if hard , change random 1.8-2.5 y. The set count by countObstacle,   1 2 3 is easy, 4 and 5 is hard

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class ObstacleSpawner : MonoBehaviour {
 
 	[SerializeField] private float waitTime;
-	[SerializeField] private GameObject[] obstaclePrefabs;
+	[SerializeField] private GameObject obstaclePrefabs;
 	private float tempTime;
+	private float lastY;
+	private int countObstacle;
 
 	void Start(){
 		tempTime = waitTime - Time.deltaTime;
+		countObstacle = 0;
+		lastY = 0;
 	}
 
 	void LateUpdate () {
@@ -18,11 +22,61 @@ public class ObstacleSpawner : MonoBehaviour {
 			if(tempTime > waitTime){
 				// Wait for some time, create an obstacle, then set wait time to 0 and start again
 				tempTime = 0;
-				GameObject pipeClone = Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)], transform.position, transform.rotation);
+				
+				SpawnObjectObstacle();
 			}
 		}
 	}
+	void SpawnObjectObstacle()
+	{
 
+		countObstacle += 1;
+		GameObject pipeClone = Instantiate(obstaclePrefabs, transform.position, transform.rotation);
+		float newY = 0;
+		float addY = 0;
+		if (IsHardObstacle())
+		{
+			//hard was diff each obstacle rd form 1.8-2.5
+			addY = (Random.Range(180, 250) * 0.01f) * (Random.value > 0.5f ? 1 : -1);
+			//Debug.LogError("Spawn Hard + addY: " + addY);
+		}
+		else
+		{
+			addY = (Random.Range(30, 100) * 0.01f) * (Random.value > 0.5f ? 1 : -1);
+			// easy was diff 0.3-1.00
+			//Debug.LogError("Spawn Easy + addY: " + addY);
+		}
+		newY = lastY +  addY;
+		//Max 2.5, change direction when top or down the map.
+		if (newY > 2.5f)
+		{
+			newY -= Mathf.Abs(addY) * 2;
+		} 
+		if (newY < -2.5f)
+		{
+			newY += Mathf.Abs(addY)  * 2;
+		} 
+		Vector3 localPosition = pipeClone.transform.localPosition;
+		localPosition = new Vector3(localPosition.x,newY,localPosition.z);
+		pipeClone.transform.localPosition = localPosition;
+		lastY = newY;
+	}
+
+	bool IsHardObstacle()
+	{
+		if (countObstacle <=3)
+		{
+			//0 1 2 is Easy
+			return false;
+		}
+		else
+		{
+			if (countObstacle >= 5) countObstacle = 0;
+			return true;
+		}
+
+
+	}
 	void OnTriggerEnter2D(Collider2D col){
 		if(col.gameObject.transform.parent != null){
 			Destroy(col.gameObject.transform.parent.gameObject);
